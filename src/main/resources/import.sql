1248fb3039b6468c17b779dad67df38125802ce8
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-16 15:26:02 -0300

    Project started

---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Silva', '123.456.789-00', 5000, '1990-05-15', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Oliveira', '234.567.890-01', 7000, '1985-11-03', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Santos', '345.678.901-02', 4000, '1995-02-28', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael Souza', '456.789.012-03', 8000, '1982-09-12', 3);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Juliana Lima', '567.890.123-04', 6000, '1988-12-24', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rodrigo Mendes', '678.901.234-05', 9000, '1977-07-05', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Camila Pereira', '789.012.345-06', 5500, '1993-03-18', 1);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Costa', '890.123.456-07', 6500, '1980-06-10', 2);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabela Torres', '901.234.567-08', 3500, '1998-08-20', 0);
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Martins', '012.345.678-09', 7500, '1987-01-30', 1);
